3016107bc00ccd755c04aad0264cfc570206b1ae
luoxuwei <xwluo@foxmail.com>
2022-07-13 23:13:54 +0800

    feat:完成设置库存和获取库存的功能

--- shop-srvs/inventory-srv/handler/inventory.go
+++ shop-srvs/inventory-srv/handler/inventory.go
@@ -1,6 +1,12 @@
 package handler
 
 import (
+	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"shop-srvs/inventory-srv/global"
+	"shop-srvs/inventory-srv/model"
 	"shop-srvs/inventory-srv/proto"
 )
 
@@ -8,3 +14,25 @@ type InventoryServer struct {
 	proto.UnimplementedInventoryServer
 }
 
+
+func (*InventoryServer) SetInv(ctx context.Context, req *proto.GoodsInvInfo) (*emptypb.Empty, error) {
+	//设置库存， 如果我要更新库存
+	var inv model.Inventory
+	global.DB.Where(&model.Inventory{Goods:req.GoodsId}).First(&inv)
+	inv.Goods = req.GoodsId
+	inv.Stocks = req.Num
+
+	global.DB.Save(&inv)
+	return &emptypb.Empty{}, nil
+}
+
+func (*InventoryServer) InvDetail(ctx context.Context, req *proto.GoodsInvInfo) (*proto.GoodsInvInfo, error) {
+	var inv model.Inventory
+	if result := global.DB.Where(&model.Inventory{Goods:req.GoodsId}).First(&inv); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "没有库存信息")
+	}
+	return &proto.GoodsInvInfo{
+		GoodsId: inv.Goods,
+		Num: inv.Stocks,
+	}, nil
+}
