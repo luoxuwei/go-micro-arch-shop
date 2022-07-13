3c44e8e3b9e503442afd4e07cc0e2871046c78b3
luoxuwei <xwluo@foxmail.com>
2022-07-13 23:57:26 +0800

    feat:完成库存减扣功能

--- shop-srvs/inventory-srv/handler/inventory.go
+++ shop-srvs/inventory-srv/handler/inventory.go
@@ -36,3 +36,29 @@ func (*InventoryServer) InvDetail(ctx context.Context, req *proto.GoodsInvInfo) 
 		Num: inv.Stocks,
 	}, nil
 }
+
+
+func (*InventoryServer) Sell(ctx context.Context, req *proto.SellInfo) (*emptypb.Empty, error) {
+
+	//对订单里的商品库存进行减扣，要么全部成功，要么全部失败，所以需要用到事务，目前还只是本机事务，最终要用分布式事务
+	tx := global.DB.Begin()
+	for _, goodInfo := range req.GoodsInfo {
+		var inv model.Inventory
+		if result := global.DB.First(&inv, goodInfo.GoodsId); result.RowsAffected == 0 {
+			tx.Rollback() //回滚
+			return nil, status.Errorf(codes.InvalidArgument, "没有库存信息")
+		}
+
+		if inv.Stocks < goodInfo.Num {
+			tx.Rollback() //回滚
+			return nil, status.Errorf(codes.ResourceExhausted, "库存不足")
+		}
+
+		//减扣操作是典型的并发问题，需要保证并发安全，不然会出现数据不一致的问题
+		//数据不一致问题，不是事务解决的问题，是锁，最终要用分布式锁
+		inv.Stocks -= goodInfo.Num
+		tx.Save(&inv)
+	}
+    tx.Commit()
+    return &emptypb.Empty{}, nil
+}
