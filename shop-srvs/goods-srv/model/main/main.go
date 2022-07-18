be448639cbdd3160f784ce926c3368e97bc1ea3d
luoxuwei <xwluo@foxmail.com>
2022-07-19 00:41:48 +0800

    feat:将mysql中的商品数据同步到es中

--- shop-srvs/goods-srv/model/main/main.go
+++ shop-srvs/goods-srv/model/main/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
+	"github.com/olivere/elastic/v7"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
+	"shop-srvs/goods-srv/global"
+	"strconv"
 
 	"log"
 	"os"
@@ -13,7 +17,7 @@ import (
 	"shop-srvs/goods-srv/model"
 )
 
-func main() {
+func m1() {
 	dsn := "root:root@tcp(192.168.139.9:3306)/shop_goods_srv?charset=utf8mb4&parseTime=True&loc=Local"
 
 	newLogger := logger.New(
@@ -39,4 +43,62 @@ func main() {
 	// 迁移 schema
 	_ = db.AutoMigrate(&model.Category{}, &model.Brands{}, &model.GoodsCategoryBrand{}, &model.Banner{}, &model.Goods{})
 
+}
+
+func main() {
+	dsn := "root:root@tcp(192.168.139.9:3306)/shop_goods_srv?charset=utf8mb4&parseTime=True&loc=Local"
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold: time.Second,   // 慢 SQL 阈值
+			LogLevel:      logger.Info, // Log level
+			Colorful:      true,         // 禁用彩色打印
+		},
+	)
+
+	// 全局模式
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	host := "http://192.168.139.9:9200"
+	logger := log.New(os.Stdout, "shop", log.LstdFlags)
+	global.EsClient, err = elastic.NewClient(elastic.SetURL(host), elastic.SetSniff(false),
+		elastic.SetTraceLog(logger))
+	if err != nil {
+		panic(err)
+	}
+
+	var goods []model.Goods
+	db.Find(&goods)
+	for _, g := range goods {
+		esModel := model.EsGoods{
+			ID:          g.ID,
+			CategoryID:  g.CategoryID,
+			BrandsID:    g.BrandsID,
+			OnSale:      g.OnSale,
+			ShipFree:    g.ShipFree,
+			IsNew:       g.IsNew,
+			IsHot:       g.IsHot,
+			Name:        g.Name,
+			ClickNum:    g.ClickNum,
+			SoldNum:     g.SoldNum,
+			FavNum:      g.FavNum,
+			MarketPrice: g.MarketPrice,
+			GoodsBrief:  g.GoodsBrief,
+			ShopPrice:   g.ShopPrice,
+		}
+
+		_, err = global.EsClient.Index().Index(esModel.GetIndexName()).BodyJson(esModel).Id(strconv.Itoa(int(g.ID))).Do(context.Background())
+		if err != nil {
+			panic(err)
+		}
+	}
 }
